2ac24fd30248eb981d5dfb96255a7b8fd00fea6d
yew1eb <yew1eb@gmail.com>
2016-01-22 20:12:29 +0800

    添加 java网络编程代码

--- Java-Notes/java.md
+++ Java-Notes/java.md
@@ -2,7 +2,16 @@
 * [JAVA/C++研发工程师学习计划_牛客网](http://www.nowcoder.com/plan/2016-springintern)  
 * [Java 编程简介，第1 部分: Java 语言基础](http://www.ibm.com/developerworks/cn/java/j-introtojava1/)
 * [Java 编程简介，第2 部分: 构建真实的应用程序](http://www.ibm.com/developerworks/cn/java/j-introtojava2/)   
+* [Java Concurrency / Multithreading Tutorial](http://tutorials.jenkov.com/java-concurrency/index.html)  
+* [Java Multithreaded Servers](http://tutorials.jenkov.com/java-multithreaded-servers/index.html)  
 
-套接字编程、非阻塞通信、数据报通信、对象的序列化与反序列化、Java反射机制
+
+1. 对象的序列化与反序列化
+2. Java反射机制
+
+套接字编程
+非阻塞通信
+数据报通信
+
 
 
---
+++ src/PostSanner.java
@@ -0,0 +1,31 @@
+import java.net.Socket;
+import java.io.IOException;
+
+/**
+ * Created by yew1eb on 2016/1/22 0022.
+ * Socket编程 扫描端口
+ */
+public class PostSanner {
+    public static void main(String args[]) {
+        String host = "localhost";
+        if( args.length > 0) host = args[0];
+        new PostSanner().scan(host);
+    }
+    public void scan(String host) {
+        Socket socket = null;
+        for(int port=1; port<1024; ++port) {
+            try {
+                socket = new Socket(host, port);
+                System.out.println("There is a server on port " + port);
+            } catch (IOException e) {
+                System.out.println("Can't connect to port " + port);
+            } finally {
+                try {
+                    if(socket != null) socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
