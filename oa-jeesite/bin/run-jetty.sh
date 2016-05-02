d98e1580d65c0c5015b0509d8d98752d304a7032
zhoouhai02 <zhouhai02@meituan.com>
2016-05-02 10:54:12 +0800

    clean

--- oa-jeesite/bin/run-jetty.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-echo "[Info] Using Jetty plugin to run project."
-
-set MAVEN_OPTS=%MAVEN_OPTS% -Xms256m -Xmx512m -XX:PermSize=128m -XX:MaxPermSize=256m
-
-mvn clean jetty:run
--- oa-jeesite/bin/run-tomcat7.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-echo "[Info] Using Tomcat7 plugin to run project."
-
-set MAVEN_OPTS=%MAVEN_OPTS% -Xms256m -Xmx512m -XX:PermSize=128m -XX:MaxPermSize=256m
-
-mvn clean tomcat7:run
