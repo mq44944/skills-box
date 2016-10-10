302c5081b7b94d5b0bf30b8c46b303f78f1431bd
zhouhai02 <zhouhai02@meituan.com>
2016-10-10 23:42:14 +0800

    update

--- Java/java-core/src/main/java/com/yew1eb/core/collection/ArraysT.java
+++ Java/java-core/src/main/java/com/yew1eb/core/collection/ArraysT.java
@@ -1,12 +1,17 @@
 package com.yew1eb.core.collection;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ArraysT {
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
         String str = "hostName";
@@ -29,5 +34,19 @@ public class ArraysT {
 
         Object x = JSON.toJSONString(list);
         System.out.println(x);
+        try {
+
+            String dateFromStr = "2016-10-10 00:00:00";
+            Date dateFrom = dateFormat.parse(dateFromStr);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(dateFrom);
+            calendar.add(Calendar.DAY_OF_MONTH, 1);//加一天
+            Date dateTo = calendar.getTime();
+            System.out.println(dateFormat.format(dateFrom));
+            System.out.println(dateFormat.format(dateTo));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 }
